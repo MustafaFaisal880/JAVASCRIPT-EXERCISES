c5307cffa45490df2591969f01a7a28aefbfd9d9
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 20:08:51 +0500

    Update Exercise (36).js

--- JAVASCRIPT EXERCISES/Exercise (36).js
+++ JAVASCRIPT EXERCISES/Exercise (36).js
@@ -1,23 +1,20 @@
-// Album: Write a function called make_album() that builds a
-// describing a music album. The function should take in an artist
-// name and an album title, and it should return a Object containing
-// these two pieces of information. Use the function to make three dictionaries
-// representing different albums. Print each return value to show that Objects
-// are storing the album information correctly. Add an optional parameter to make_album()
+// Album: Write a function called make_album() that builds a describing a music album. The function should take in an artist
+// name and an album title, and it should return a Object containing these two pieces of information. 
+// Use the function to make three dictionaries representing different albums. 
+// Print each return value to show that Objects are storing the album information correctly. Add an optional parameter to make_album()
 // that allows you to store the number of tracks on an album. If the calling line includes
 // a value for the number of tracks, add that value to the album’s Object.
 // Make at least one new function call that includes the number of tracks on an album.
 
-
-
-function make_album(artistName,albumName,noOfSongs){
-let musicAlbum={
-    artistName,
-    albumName,
-    noOfSongs
-}
-console.log(musicAlbum)
-}
-make_album("ALI ZAFAR","JHOOM",1)
-make_album("ALI ZAFAR","MASTY",2)
-make_album("ALI ZAFAR","TEEFA",3)
+function make_album(artistName,albumTitle,noSong){
+    let musicAlbum={
+        artistName,
+        albumTitle,noSong
+    }
+    console.log(musicAlbum)
+    }
+    console.log("It is returning a Object containing two pieces of information.")
+    make_album("ALI ZAFAR","JHOOM")
+    make_album("ALI ZAFAR","MASTY")
+    console.log("It is returning a Object with no of tracks also")
+    make_album("ALI ZAFAR","TEEFA",3)
