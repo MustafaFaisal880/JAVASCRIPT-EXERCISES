55e4b181247a48037c015837f8dbc9d9f98b58d0
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 18:20:37 +0500

    Update Exercise (18).js

--- JAVASCRIPT EXERCISES/Exercise (18).js
+++ JAVASCRIPT EXERCISES/Exercise (18).js
@@ -2,10 +2,6 @@
 // print a message indicating the number of people you are inviting to dinner.
 //
 
-// Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
-// print a message indicating the number of people you are inviting to dinner.
-//
-
 var invite=["KAMRAN","HASNAIN","FAHAD","ADNAN","ALI","HAQ"]
 console.log(invite)
 let l=0
