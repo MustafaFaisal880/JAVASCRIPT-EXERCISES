3adb629732177e5831b8103765624e1f131b6799
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 17:41:24 +0500

    Update Exercise (14).js

--- JAVASCRIPT EXERCISES/Exercise (14).js
+++ JAVASCRIPT EXERCISES/Exercise (14).js
@@ -1,22 +1,10 @@
-// Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
-//
-// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
-//
-// • Print a second set of invitation messages, one for each person who is still in your list.
+// You should create four lines that look like this:
+// console.log(5 + 3)
 
-console.log(" \n \tGANDHI CAN'T MAKE IT TO THE PARTY :(")
-console.log(" \n \tSO SENDIND NEW INVITES: ")
-
-
-for( var i = 0; i < invites.length; i++){
-
-    if ( invites[i] === "TESLA") {
-
-        invites.splice(i, 1,"MUSTAFA");
-    }
-
-}
-for(var i=0 ;i<invites.length;i++)
-{
-    console.log(`\n \tHi ${invites[i]} you are invited to my another code party`)
-}
+console.log(5 + 3 )
+console.log("\n")
+console.log(5 + 3)
+console.log("\n")
+console.log(5 + 3)
+console.log("\n")
+console.log(5 + 3)
