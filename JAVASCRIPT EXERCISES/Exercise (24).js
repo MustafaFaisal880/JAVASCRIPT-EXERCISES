1e9e08dd36a539c6c6fef6f775bbd5e63cbe29ae
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 19:21:54 +0500

    Update Exercise (24).js

--- JAVASCRIPT EXERCISES/Exercise (24).js
+++ JAVASCRIPT EXERCISES/Exercise (24).js
@@ -1,27 +1,27 @@
 // Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
 // • Make a array of your three favorite fruits and call it favorite_fruits.
-//
-// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, 
+// the if block should print a statement, such as You really like bananas!
 
-var favorite_Fruit=["banana","apple","kiwi","mango","orange"]
+var favorite_Fruit=["banana","apple","orange","KIWI"]
 console.log(favorite_Fruit)
 
 for(var i=0;i<favorite_Fruit.length;i++)
 {
-    if(favorite_Fruit[i].toLowerCase()=='kiwi')
+    if(favorite_Fruit[i].toLowerCase()=='banana')
     {
         console.log(`You love ${favorite_Fruit[i]}`)
     }
-    else if(favorite_Fruit[i].toLowerCase()=='mango')
+    else if(favorite_Fruit[i].toLowerCase()=='apple')
     {
         console.log(`You love ${favorite_Fruit[i]}`)
     }
-    else if(favorite_Fruit[i].toUpperCase()=='BANANA')
+    else if(favorite_Fruit[i].toUpperCase()=='ORANGE')
     {
         console.log(`You love ${favorite_Fruit[i]}`)
     }
     else
     {
-        console.log(`You love ${favorite_Fruit[i]}`)
+        console.log(`${favorite_Fruit[i]} IS NOT IN YOUR LIST`)
     }
 }
