c2de9ca85c0f67f6ada2e10630c4ef06118cc7e8
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 19:37:29 +0500

    Update Exercise (30).js

--- JAVASCRIPT EXERCISES/Exercise (30).js
+++ JAVASCRIPT EXERCISES/Exercise (30).js
@@ -1,12 +1,28 @@
-// Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common.
-//     You could print a sentence such as Any of these animals would make a great pet!
+// Animals: Think of at least three different animals that have a common characteristic.
+// Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+// • Modify your program to print a statement about each animal, such as A dog would make a great pet. 
+// • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
 
 
 
 var animal=["CAT","LION","TIGER"]
 for(var i=0;i<animal.length;i++)
 {
-    console.log("\n"+animal[i])
-    console.log(`${animal[i]} is a great pet`)
+    console.log(animal[i])
 }
-console.log("\n"+animal[0]+" would be okay to be around")
+for(var i=0;i<animal.length;i++)
+{
+    if(animal[i]=="CAT")
+       {
+       console.log("I LOVE CATS")
+       }
+
+       else if(animal[i]=="LION")
+       {
+       console.log("LION ARE DANGEROUS")
+       }
+       else{
+        console.log("TIGERS ARE GREAT PREDATORS")
+       }
+}
+
