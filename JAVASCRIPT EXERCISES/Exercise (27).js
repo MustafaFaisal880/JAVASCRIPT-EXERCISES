42404bdc05806658f6f545c0e7e87f7bdcc61acc
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 19:28:33 +0500

    Update Exercise (27).js

--- JAVASCRIPT EXERCISES/Exercise (27).js
+++ JAVASCRIPT EXERCISES/Exercise (27).js
@@ -1,14 +1,13 @@
 // Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
 // • Make a list of five or more usernames called current_users.
-//
 // • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
-//
-// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
-//
+// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. 
+// If a username has not been used, print a message saying that the username is available.
 // • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
 
 var currentUser=["ADMIN","MUSHI","KAMI","mustafa"]
-var newUsers=["ADMIN2","mushi","kamran","MUSTAFA"]
+var newUsers=["ADMIN2","MUSTAFA"]
+console.log("FOR NOT AVAIABLE USERNAMES")
 for(var i=0;i<currentUser.length;i++)
 {
     for(var j=0;j<newUsers.length;j++)
@@ -16,4 +15,11 @@ for(var i=0;i<currentUser.length;i++)
         if(currentUser[i].toLowerCase()===newUsers[j].toLowerCase())
             console.log("\nTRY A DIFFERENT USERNAME"+" INSTEAD OF "+newUsers[j])
     }
+}
+
+console.log("FOR AVAIABLE USERNAMES")
+for(var i=0;i<currentUser.length;i++)
+{
+        if(currentUser[i].toLowerCase()!==newUsers[i].toLowerCase())
+            console.log("\nUSERNAME AVAILABLE "+ newUsers[i])
 }
