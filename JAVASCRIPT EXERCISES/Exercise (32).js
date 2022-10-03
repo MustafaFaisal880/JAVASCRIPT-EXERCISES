8beb23c6f3cb1851c2ccfc42f61cc2744c1c0c33
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 19:51:51 +0500

    Update Exercise (32).js

--- JAVASCRIPT EXERCISES/Exercise (32).js
+++ JAVASCRIPT EXERCISES/Exercise (32).js
@@ -1,17 +1,20 @@
-// Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value.
-//     Call your function for three different cities, at least one of which is not in the default country.
+// Cities: Write a function called describe_city() that accepts the name of a city and its country. 
+// The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value.
+// Call your function for three different cities, at least one of which is not in the default country.
+
+
 function describe_city(country,city="KARACHI"){
-console.log("THE NAME OF CITY: "+city)
-console.log("THE NAME OF COUNTRY:"+country)
-console.log(`${city} is in ${country}`)
-}
-describe_city("PAKISTAN")
-
-function city_country(country,city){
-
-    console.log(`"${country}, ${city}"`)
-}
-
-city_country("KARACHI","PAKISTAN")
-city_country("ITALY","FRANCE")
-city_country("MUMBAI","INDIA")
+    console.log("ONE WITH DEFAULT COUNTRY PARAMETER: ")
+    console.log("THE NAME OF CITY: "+city)
+    console.log("THE NAME OF COUNTRY:"+country)
+    }
+    describe_city("PAKISTAN")
+    
+    function city_country(country,city){
+    
+        console.log(`"${country}, ${city}"`)
+    }
+    
+    city_country("KARACHI","PAKISTAN")
+    city_country("ITALY","FRANCE")
+    city_country("MUMBAI","INDIA")
