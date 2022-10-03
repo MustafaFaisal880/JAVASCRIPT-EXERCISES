9278618099ac7cffe7f55063344bddf684679e37
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 17:10:36 +0500

    Update Exercise (8).js

--- JAVASCRIPT EXERCISES/Exercise (8).js
+++ JAVASCRIPT EXERCISES/Exercise (8).js
@@ -3,14 +3,16 @@
 // to write because your programs are too simple at this point, just add your
 // name and the current date at the top of each program file. Then write one
 // sentence describing what the program does.
-//
 
 
+// NAME: MUSTAFA FAISAL
+// DATE: 29-September-2022
+// THIS IS A PROGRAM OF OUTPUT
 
-
-// // NAME: MUSTAFA FAISAL
-// // DATE: 29-September-2022
-// // THIS IS A PROGRAM OF OUTPUUT
-//  var famous_person ='Albert Einstein once said: '
-//  var message ='"A person who never made a mistake never tried anything new."'
-//  console.log(famous_person + message);
+// I HAVE CREATED AN ARRAY NAMES AND CALLED THEM THROUGH INDEX METHOD
+const names=['KHURRAM','KAMRAN','ADNAN','KASHAN','SHEES']
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+console.log(names[4])
