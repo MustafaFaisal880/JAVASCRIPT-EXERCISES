6dbc30525dc804fea2ddee75b78958a90d59daa9
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 19:23:39 +0500

    Update Exercise (25).js

--- JAVASCRIPT EXERCISES/Exercise (25).js
+++ JAVASCRIPT EXERCISES/Exercise (25).js
@@ -1,6 +1,7 @@
-// Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
-//     • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
-//
+// Hello Admin: Make a array of five or more usernames, including the name 'admin'. 
+// Imagine you are writing code that will print a greeting to each user after they log in to a website. 
+// Loop through the array, and print a greeting to each user:
+//  • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 // • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
 
 var currentUser=["ADMIN","MUSHI","KAMI","MUSTAFA"]
@@ -9,16 +10,8 @@ for(var i=0;i<currentUser.length;i++)
  {
      if(currentUser[i].toLowerCase()=='admin')
     {
-        console.log(`Hello ${currentUser[i]}`)
+        console.log(`Hello ${currentUser[i]}, WELCOME BACK`)
      }
-     else if(currentUser[i].toLowerCase()=='mushi')
-     {
-         console.log(`HI ${currentUser[i]}, thank you for logging in again`)
-     }
-     else if(currentUser[i].toUpperCase()=='kami')
-    {
-         console.log(`HI ${currentUser[i]}, thank you for logging in again`)
-    }
      else
      {
         console.log(`HI ${currentUser[i]}, thank you for logging in again`)
