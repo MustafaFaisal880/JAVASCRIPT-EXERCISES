763d1a28dc0b00ba4269f82883e44e318a3e64ee
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 17:58:50 +0500

    Update Exercise (15).js

--- JAVASCRIPT EXERCISES/Exercise (15).js
+++ JAVASCRIPT EXERCISES/Exercise (15).js
@@ -1,40 +1,31 @@
-// More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
-// • Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+// More Guests: You just found a bigger dinner table, so now more space is available. 
+// Think of three more guests to invite to dinner.
+// • Start with your program from Exercise 15. Add a print statement to the end of your program 
+// informing people that you found a bigger dinner table.
 //
 // • Add one new guest to the beginning of your array.
 //
-// • Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.
+// • Add one new guest to the middle of your array. • Use append() to add one new guest to the 
+// end of your list. • Print a new set of invitation messages, one for each person in your list.
 
-const invites = ['GANDHI','ALBERT EINSTEIN','TESLA']
-for(var i=0 ;i<invites.length;i++)
-{
-    console.log(`\nHi ${invites[i]} you are invited to my code party`)
-}
-
-
-console.log(" \n \t***** GANDHI CAN'T MAKE IT TO THE PARTY :( *****")
-console.log(" \n \t ****** SO SENDIND NEW INVITES: *****")
-
-
-for( var i = 0; i < invites.length; i++){
-
-    if ( invites[i] === "TESLA") {
-
-        invites.splice(i, 1,"MUSTAFA");
-    }
-
-}
-for(var i=0 ;i<invites.length;i++)
-{
-    console.log(`\n \tHi ${invites[i]} you are invited to my another code party`)
-}
-
+let invites = ['GANDHI','ALBERT','TESLA']
+console.log(invites)
+console.log("I FOUND A BIGGER TABLE!")
 console.log(" \n***** MAKE ROOM FOR 3 MORE GUESTS ******")
 
+
+console.log('ADDING HASNAIN TO THE LAST OF ARRAY')
 invites.push("HASNAIN")
+
+console.log("ADDING FAHAD IN BEGINNING")
 invites.unshift("FAHAD")
-invites.splice(4,1,"SHEES")
+
+console.log("ADDING SHEES IN MIDDLE")
+invites.splice(2,0,"SHEES")
+
 console.log(invites)
+
+console.log(" \n \t ****** SO SENDIND NEW INVITES: *****")
 for(var i=0 ;i<invites.length;i++)
 {
     console.log(`\n \tHi ${invites[i]} you are invited to my another code party`)
