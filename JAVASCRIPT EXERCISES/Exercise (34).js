4884d501ad5c2b3fd581f3068b03c863e19affb6
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 19:56:53 +0500

    Update Exercise (34).js

--- JAVASCRIPT EXERCISES/Exercise (34).js
+++ JAVASCRIPT EXERCISES/Exercise (34).js
@@ -1,5 +1,7 @@
-// Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich
-// that is being ordered. Call the function three times, using a different number of arguments each time.
+// Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. 
+// The function should have one parameter that collects as many items as the function call provides, 
+// and it should print a summary of the sandwich that is being ordered. 
+// Call the function three times, using a different number of arguments each time.
 
 function sandwich(){
     console.log("\nTHESE ARE ALL YOUR ITEMS ON SANDWICH: ")
@@ -9,6 +11,8 @@ function sandwich(){
     }
 }
 sandwich(items=["BREAD","CHEESE","KETCHUP"])
+console.log("\n*****CALLING IT THREE MORE TIMES*****")
 sandwich(items=["PEPPER","TOMATO","CHICKEN"])
 sandwich(items=["MAYO","FAJITA","LETTUCE"])
+sandwich(items=["MAYO","FAJITA","LETTUCE","PEPPER","TOMATO","CHICKEN","BREAD","CHEESE","KETCHUP"])
 
