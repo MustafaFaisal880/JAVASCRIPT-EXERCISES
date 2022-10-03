6b9182a4118f139dcf690f0b515ab78758af92f5
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-03 18:48:32 +0500

    Update Exercise (20).js

--- JAVASCRIPT EXERCISES/Exercise (20).js
+++ JAVASCRIPT EXERCISES/Exercise (20).js
@@ -1,27 +1,9 @@
 // Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
-//
 // let car = 'subaru';
-//
 // console.log("Is car == 'subaru'? I predict True.")
-//
 // console.log(car == 'subaru')
-//
 // • Look closely at your results, and make sure you understand why each line evaluates to True or False.
-//
 // • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
-//
-//     More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
-//     • Tests for equality and inequality with strings
-//
-//     • Tests using the lower case function
-//
-// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
-//
-// • Tests using "and" and "or" operators
-//
-// • Test whether an item is in a array
-//
-// • Test whether an item is not in a array
 
 let car = 'subaru';
 console.log("Is car == 'subaru'? I predict True.")
@@ -42,27 +24,3 @@ console.log(people === 10)
 let num = '1';
 console.log("Is num == 'subaru'? I predict True.")
 console.log(num === '1')
-
-let no = '1';
-console.log("Is no == 'subaru'? I predict False.")
-console.log(no === 1)
-
-let us = 'hand';
-console.log("Is us == 'subaru'? I predict True.")
-console.log(us == 'hand')
-
-let No = ['subaru'];
-console.log("Is No == 'subaru'? I predict true.")
-console.log(No !== no[0])
-
-let trans = ['subaru','honda'];
-console.log("Is trans[1] !== trans[0]? I predict true.")
-console.log(trans[1] !== trans[0])
-
-let Uo = ['subaru'];
-console.log("Uo !== Uo[0]? I predict false.")
-console.log(Uo != Uo[0])
-
-let mustafa = 1
-console.log("mustafa === 1? I predict true.")
-console.log(mustafa === 1)
