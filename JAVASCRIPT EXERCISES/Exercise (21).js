88a24bce5632ee92e7e07b2a6e3831f9e1f2436d
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-13 12:58:15 +0500

    Update Exercise (21).js

--- JAVASCRIPT EXERCISES/Exercise (21).js
+++ JAVASCRIPT EXERCISES/Exercise (21).js
@@ -2,13 +2,6 @@
 // • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
 //
 // • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
-//
-// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
-// • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
-//
-// • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
-//
-// • Write one version of this program that runs the if block and another that runs the else block.
 
 let alien_color='green'
     if(alien_color=='green')
@@ -17,18 +10,6 @@ let alien_color='green'
     }
     else
     {
-        console.log("\nYOU EARNED 10 POINTS ")
+        console.log("\nNO OUTPUT")
     }
-     alien_color='yellow'
-    if(alien_color=='yellow')
-    {
-        console.log('\nYOU EARNED 10 POINTS')
-    }
-    
 
-console.log("ANOTHER VERSION WITH NO OUTPUT")
-for(var j=0;i<alien_color.length;j++)
-{
-    console.log(alien_color[j]=='violet'? "YOU GOT 5 POINTS":"\nGOT NOTHING")
-    break;
-}
