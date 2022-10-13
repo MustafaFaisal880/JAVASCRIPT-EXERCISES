6f48dbfdb259363b613810c2bb2aedf255ba7df4
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-13 13:18:07 +0500

    Update Exercise (20).js

--- JAVASCRIPT EXERCISES/Exercise (20).js
+++ JAVASCRIPT EXERCISES/Exercise (20).js
@@ -1,4 +1,5 @@
-// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. 
+//Your code should look something like this:
 // let car = 'subaru';
 // console.log("Is car == 'subaru'? I predict True.")
 // console.log(car == 'subaru')
@@ -15,12 +16,12 @@ console.log(bus == 'CAR')
 
 let boy = 'boy';
 console.log("Is boy == 'subaru'? I predict True.")
-console.log(boy == 'boy')
+console.log(boy === 'boy')
 
-let people = 'subaru';
-console.log("Is people == 'subaru'? I predict False.")
-console.log(people === 10)
+let people = 'civic';
+console.log("Is people == 'six'? I predict False.")
+console.log(people === 'six')
 
 let num = '1';
-console.log("Is num == 'subaru'? I predict True.")
+console.log("Is num == '1'? I predict True.")
 console.log(num === '1')
