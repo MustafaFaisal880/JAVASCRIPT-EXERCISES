f17c967513980997d71d8e8db5e456d2d4dae6d1
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-13 13:32:55 +0500

    Add files via upload

---
+++ JAVASCRIPT EXERCISES/Exercise (41).js
@@ -0,0 +1,51 @@
+// More Conditional Tests: You don’t have to limit the number of tests 
+// you create to 10. If you want to try more comparisons, 
+// write more tests. Have at least one True and one False 
+// result for each of the following:
+// • Tests for equality and inequality with strings
+// • Tests using the lower case function
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+// • Tests using "and" and "or" operators
+// • Test whether an item is in a array
+// • Test whether an item is not in a array
+
+
+// • Tests for equality and inequality with strings
+let car = 'bus'
+console.log("car == 'bus' ? I PREDICT TRUE")
+console.log(car == 'bus')
+
+car =='bus'
+console.log("car != 'bus' ? I PREDICT FALSE")
+console.log(car != 'bus')
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+let no = 2
+console.log("no !=2  ? I PREDICT FALSE")
+console.log(no != 2)
+no=2
+console.log("no <=2  ? I PREDICT TRUE")
+console.log(no <= 2)
+no=2
+console.log("no > 2  ? I PREDICT FALSE")
+console.log(no > 2)
+// • Tests using the lower case function
+
+let capital="JAVASCRIPT"
+console.log("capital == 'JAVASCRIPT' ? I PREDICT TRUE")
+console.log(capital == 'JAVASCRIPT')
+
+console.log("capital == 'javascript' ? I PREDICT FALSE")
+console.log(capital == 'javscript')
+
+
+// • Tests using "and" and "or" operators
+let a = 2
+let b=3
+console.log("a ==2 && b==3 ? I PREDICT TRUE")
+console.log(a ==2 && b==3)
+
+b=4
+console.log("a ==2 && b==4 ? I PREDICT FALSE")
+console.log(a ==2 && b==3)
+
+
