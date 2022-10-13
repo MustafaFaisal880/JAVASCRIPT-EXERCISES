656d2ebeb9413c40818ed8b3a28143887d46878f
Mustafa Faisal <113097716+MustafaFaisal880@users.noreply.github.com>
2022-10-13 13:46:10 +0500

    Add files via upload

---
+++ JAVASCRIPT EXERCISES/Exercise (42).js
@@ -0,0 +1,9 @@
+// Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.
+function describe_city(city,country="PAKISTAN"){
+    console.log("ONE WITH DEFAULT COUNTRY PARAMETER: ")
+    console.log("THE NAME OF CITY: "+city)
+    console.log("THE NAME OF COUNTRY:"+country)
+    }
+describe_city("KARACHI")
+describe_city("LAHORE")
+describe_city("ISLAMABAD")
---
+++ JAVASCRIPT EXERCISES/Exercise (43).js
@@ -0,0 +1,20 @@
+// Think of something you could store in a array. 
+// For example, you could make a list of mountains,
+// rivers, countries, cities, 
+// languages, or anything else you’d like.
+// Write a program that creates a list containing these items.
+// They think of something you could store in a JavaScript Object.
+// Write a program that creates Objects containing these items.
+
+// Write a program that creates a list containing these items.
+const countries=["PAKISTAN","INDIA","AUSTRALIA","JAPAN"]
+console.log(countries)
+
+// Write a program that creates Objects containing these items.
+country={
+    First: "PAKISTAN",
+    Second:"INDIA",
+    Third:"AUSTRALIA",
+    Fourth:"JAPAN"
+}
+console.log(country)
